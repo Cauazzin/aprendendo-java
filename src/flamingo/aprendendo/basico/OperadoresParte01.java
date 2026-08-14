99dcd2719ba00dccc1862f0323e8b4cce7ad24cc
Cauã Ferreira Santos <cauafsantos17@gmail.com>
2026-08-13 21:06:35 -0300

    Add files via upload

---
+++ src/flamingo/aprendendo/basico/OperadoresParte01.java
@@ -0,0 +1,29 @@
+package flamingo.aprendendo.basico;
+
+public class OperadoresParte01 {
+    public static void main(String[] args) {
+        /*
+        *   + Soma
+        *   - Subtração
+        *   / Divisão
+        *   * Multiplicação
+        *   % resto da divisão
+        * */
+        //int numero01 = 200;
+        //int numero02 = 120;
+        //int soma = numero01 + numero02;
+
+        //System.out.println("A soma dos numeros = " + soma);
+
+        /* System.out.println(10 + 10);
+        System.out.println(10 - 10);
+        System.out.println(10 / 10);
+        System.out.println(10 * 10);
+        System.out.println(10 % 10); */
+
+        int primeiroNumero = 10;
+        int segundoNumero = 20;
+
+        System.out.println((double) primeiroNumero / segundoNumero);
+    }
+}
--- src/flamingo/aprendendo/introducao/ExercicioPraticaBasica.java
+++ src/flamingo/aprendendo/introducao/ExercicioPraticaBasica.java
@@ -4,7 +4,7 @@ public class ExercicioPraticaBasica {
     public static void main(String[] args) {
         String nome = "Cauã";
         String endereco = "Portal dos ipês - Cajamar";
-        float salario = 1234.56f;
+        double salario = 1234.5;
         String data = "12/08/26";
         System.out.println("Eu " + nome + " morando no endereço " + endereco + ", confirmo o salário de " + salario + " na data " + data );
 
